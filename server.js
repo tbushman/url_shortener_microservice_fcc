fc2133192c1fe001989099cf733a30d413802307
tbushman <tbushman@pu.bli.sh>
2016-10-07 01:55:40 -0600

    page redirects to related url in db :)

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var dotenv = require('dotenv');
+var url = require('url');
 var ObjectID = mongodb.ObjectID;
 
 var app = express();
@@ -49,23 +50,36 @@ function handleError(res, reason, message, code) {
 
 var newUrl;
 app.get('/', function(req, res){
-//app.get(/(.*)/, function (req, res) { //regexp checks if at least one char in path
-	//var inputUrl = url.parse(req.url);
 	newUrl = req.protocol + '://'+ req.get('host') + req.originalUrl;
-	//url.format(inputUrl)
-	console.log(newUrl)
-	/*if ( err) {
-		handleError(res, "Invalid URL", "Must contain ... etc", 400);
-	} */
 	res.render('index', {
 		title: 'FCC URL Shortener Microservice'//,
-		//response: formatOutput(doc)
+	});
+});
+
+
+app.get(/(.*)/, function (req, res) { //regexp checks if at least one char in path
+	var outputPath = url.parse(req.url).pathname;
+	//pathname includes slash...so remove it . 
+	//we're looking for an alpha-numeric id, i.e. 'a1', so .replace() works here
+	outputPath = outputPath.replace('/', '');
+	var collection = db.collection('fcc_urls');
+	var inputUrl;
+	collection.findOne({urlid: outputPath}, function(err, doc){
+		if (err) {
+			handleError(res, err.message, "Failed to redirect.");
+			return false;
+		} else {
+			inputUrl = ''+doc.input+'';
+		}
+		res.redirect(inputUrl);
 	});
 });
 
 app.post('/', urlencodedParser, function(req, res){ //if path empty, post from form
-	//regexp for 'http(s)://chars.'
+	//concatenated parts of host url (.herokuapp. or in build stage 'localhost:8080')
 	newUrl = req.protocol + '://'+ req.get('host') + req.originalUrl;
+	//regexp to check for 'http(s)://chars.' in input url
+	//very basic regexp compared to some 
 	var re = /(http(s)*:\/\/)+[\w]+[\.]+/;
 	var test = re.test(req.body.urli);
 	if (test == false) {
@@ -75,33 +89,42 @@ app.post('/', urlencodedParser, function(req, res){ //if path empty, post from f
 		var alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 		var alphInd;
 		var num;
-		var dblength = collection.find().count();
-		if (dblength >= alph.length) {
-			num = dblength % alph.length;
-			if (num > 9) {
-				alphInd = 1; //need to change this... thinking...
-			}
-		} else {
-			alphInd = 0;
-			num = dblength+1;
-		}
-		var shortened = ""+newUrl+""+alph[alphInd]+""+num+"";
-		var showUrl = {
-			input: req.body.urli,
-			output: shortened
-		};
-		
-		collection.insertOne(showUrl, function(err, doc){
+		var dblength;
+		collection.count(function(err, cnt){
 			if (err) {
-				handleError(res, err.message, "Failed to shorten URL.");
+				handleError(res, err.message, "Failed to sum number of records in collection.");
 			} else {
-				res.render('index', {
-					title: 'FCC URL Shortener Microservice',
-					response: formatOutput(doc.ops[0]),
-					link: req.body.urli
+				dblength = cnt;
+				if (dblength >= alph.length) {
+					num = dblength % alph.length;
+					if (num > 9) {
+						alphInd = 1; //need to change this... thinking...
+					}
+				} else {
+					alphInd = 0;
+					num = dblength+1;
+				}
+				var uniqueId = alph[alphInd]+""+num;
+				var shortened = ""+newUrl+""+uniqueId+"";
+				var showUrl = {
+					input: req.body.urli,
+					output: shortened,
+					urlid: uniqueId
+				};
+
+				collection.insertOne(showUrl, function(err, doc){
+					if (err) {
+						handleError(res, err.message, "Failed to shorten URL.");
+					} else {
+						res.render('index', {
+							title: 'FCC URL Shortener Microservice',
+							response: formatOutput(doc.ops[0]),
+							link: shortened
+						});
+
+					//	res.status(201).json(doc.ops[0]);
+					}
 				});
-				
-			//	res.status(201).json(doc.ops[0]);
 			}
 		});
 	}
